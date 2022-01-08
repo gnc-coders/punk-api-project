e4d05ebf1c0e28dbd3d497a06007e2b7c0b7f593
Nejla Şahin <2017123027@cumhuriyet.edu.tr>
2022-01-08 22:28:35 +0300

    Update Footer.jsx

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,7 +1,7 @@
 import logo from '../assets/logo.png'
 
 function Footer() {
-    
+
     return (
         <>
             <hr className="mt-5" />
@@ -13,21 +13,9 @@ function Footer() {
                     <div className="col-6 col-md">
                         <h5>Team Members</h5>
                         <ul className="list-unstyled text-small">
-                            <li><a className="link-secondary text-decoration-none text-primary" href="https://github.com/gamzegurcan" rel='noreferrer' target="_blank">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-right-short" viewBox="0 0 16 16">
-                                    <path fillRule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z" />
-                                </svg>
-                                Gamze Gürcan</a></li>
-                            <li><a className="link-secondary text-decoration-none text-primary" href="https://github.com/cansuyarkin" rel='noreferrer' target="_blank">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-right-short" viewBox="0 0 16 16">
-                                    <path fillRule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z" />
-                                </svg>
-                                Cansu Yarkın</a></li>
-                            <li><a className="link-secondary text-decoration-none text-primary" href="https://github.com/nejlasahin" rel='noreferrer' target="_blank">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-right-short" viewBox="0 0 16 16">
-                                    <path fillRule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z" />
-                                </svg>
-                                Nejla Şahin</a></li>
+                            <li><a className="link-secondary text-decoration-none text-primary" href="https://github.com/gamzegurcan" rel='noreferrer' target="_blank">Gamze Gürcan</a></li>
+                            <li><a className="link-secondary text-decoration-none text-primary" href="https://github.com/cansuyarkin" rel='noreferrer' target="_blank">Cansu Yarkın</a></li>
+                            <li><a className="link-secondary text-decoration-none text-primary" href="https://github.com/nejlasahin" rel='noreferrer' target="_blank">Nejla Şahin</a></li>
                         </ul>
                     </div>
                     <div className="col-6 col-md">

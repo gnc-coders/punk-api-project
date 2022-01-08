ccd9cadb3a156a8776c197c764fb4d359bc2d163
Nejla Şahin <2017123027@cumhuriyet.edu.tr>
2022-01-09 00:17:06 +0300

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,12 @@
+import noImage from "../assets/no-image.png"
+
 function Card(props) {
 
     return (
         <>
             <div className="col">
                 <div className="card border-0">
-                    <img src={props.image_url} height={200} className="mx-auto" alt={props.name} />
+                    <img src={props.image_url ? props.image_url : noImage} height={200} className="mx-auto" alt={props.name} />
                     <div className="card-body">
                         <h5 className="card-title">{props.name}</h5>
                         <p className="card-text">{props.first_brewed}</p>
